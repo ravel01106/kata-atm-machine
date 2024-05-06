09ae40d9dec6c5ecbbc6fc1c87f8112f40de41c1
ravel01106 <amandaravelocabrera@gmail.com>
2024-05-06 14:47:08 +0100

    Test(Green): withdraw the necessary bills and coins when the quantity entered is 434.

--- src/test/kotlin/dev/kata/atmmachine/AtmMachineShould.kt
+++ src/test/kotlin/dev/kata/atmmachine/AtmMachineShould.kt
@@ -8,9 +8,10 @@ class AtmMachineShould{
     // withdraw one five bill when the quantity entered is 5
     // withdraw one ten five bill and one five bill when the quantity entered is 15
     // withdraw two five-hundred bills when the quantity entered is 1000
-    // withdraw one twenty bill, one ten bill, two twopennies when the quantity entered is 33
+    // withdraw one twenty bill, one ten bill, two twopennies when the quantity entered is 34
+    // withdraw the necessary bills and coins when the quantity entered is 434
     // withdraw the necessary bills and coins when the quantity entered is 1725
-    var atmMachine: AtmMachine = AtmMachine()
+    private var atmMachine: AtmMachine = AtmMachine()
     @BeforeEach
     fun `reset atm machine`(){
         atmMachine = AtmMachine()
@@ -38,6 +39,15 @@ class AtmMachineShould{
         assertEquals("2 billetes de 500", result)
     }
 
-
+    @Test
+    fun `withdraw one twenty bill, one ten bill, two twopennies when the quantity entered is 34`(){
+        val result = atmMachine.withdraw(34)
+        assertEquals("1 billete de 20\n1 billete de 10\n2 monedas de 2", result)
+    }
+    @Test
+    fun `withdraw the necessary bills and coins when the quantity entered is 434`(){
+        val result = atmMachine.withdraw(434)
+        assertEquals("2 billetes de 200\n1 billete de 20\n1 billete de 10\n2 monedas de 2", result)
+    }
 
 }
